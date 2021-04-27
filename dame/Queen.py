0f671e1c03422d2ea120b18e0a850627130dd170
Lucas Vossen <lucas.vossen@hotmail.de>
2021-04-27 14:26:12 +0200

    add function get_y_direction

---
+++ dame/Queen.py
@@ -0,0 +1,15 @@
+class Queen:
+    __x = -1
+    __y = -1
+    __player = -1
+
+    def __init__(self, x, y, player):
+        self.setPosition(self, x, y)
+        self.__player = player
+
+    def set_position(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    def get_y_direction(self):
+        return 1 if self.__player == 1 else -1
