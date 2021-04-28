eab723215e6eafb5d4d298d1626aef143f0772ce
Lucas Vossen <lucas.vossen@hotmail.de>
2021-04-28 09:23:00 +0200

    modify class Move

--- dame/Move.py
+++ dame/Move.py
@@ -1,17 +1,12 @@
 class Move:
-    __player = -1
     __queen = None
     __to_row = -1
     __to_column = -1
 
-    def __init__(self, player, queen, to_row, to_column):
-        self.__player = player
+    def __init__(self, queen, to_row, to_column):
         self.__queen = queen
         self.__to_row = to_row
         self.__to_column = to_column
-
-    def get_player(self):
-        return self.__player
 
     def get_queen(self):
         return self.__queen
