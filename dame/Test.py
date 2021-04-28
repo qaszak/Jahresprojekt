4547626b84e405b5e1be7840508dbb5a110d87f1
Lucas Vossen <lucas.vossen@hotmail.de>
2021-04-28 09:25:38 +0200

    add a basic turn functionality

--- dame/Test.py
+++ dame/Test.py
@@ -1,4 +1,23 @@
 import Game
 
-game = Game.Game(6)
-game.print_board()
+game = None
+
+print("-- Welcome to Dame --\nCommands:\ns: start a new game\nm: move queen\nq: quit")
+
+while True:
+    user_input = input(": ")
+    if user_input == "s":
+        game = Game.Game(6)
+        game.print_board()
+    elif user_input == "m":
+        current_row = int(input("\tcurrent_row: "))
+        current_column = int(input("\tcurrent column: "))
+        destination_row = int(input("\tdestination row: "))
+        destination_column = int(input("\tdestination column: "))
+        game.move_queen_to(current_row, current_column, destination_row, destination_column)
+    elif user_input == "q":
+        print("Bye.")
+        break
+    else:
+        print("unknown command.\nCommands:\ns: start a new game\nm: move queen\nq: quit")
+
