e24d010e66e881e446cb56b7b382250d3054d449
Lucas Vossen <lucas.vossen@hotmail.de>
2021-04-28 09:20:54 +0200

    modify class move

--- dame/Game.py
+++ dame/Game.py
@@ -1,29 +1,32 @@
 import DameBoard
 import GameLogic
+import Move
 
 class Game:
     __internal_board = None
-    __selected_queen = None
-    __game_logic = None
+    game_logic = None
     __player_turn = -1
 
     def __init__(self, board_size):
         self.__internal_board = DameBoard.DameBoard(board_size)
-        # self.__game_logic = ...
+        self.game_logic = GameLogic.GameLogic()
         self.__player_turn = 2
-
 
     def start(self, board_size):
         pass
 
-    def select_queen(self):
-        pass
-
-    def move_selected_queen(self, x, y):
-        pass
+    def move_queen_to(self, current_row, current_column, destination_row, destination_column):
+        move = self.__create_move_object(current_row, current_column, destination_row, destination_column)
+        self.game_logic.set_move(self.__internal_board, move)
+        # self.__game_logic.print_logic_checks(self.__player_turn)
 
     def print_board(self):
         self.__internal_board.print()
+
+    # private methods
+    def __create_move_object(self, current_row, current_column, destination_row, destination_column):
+        queen = self.__internal_board.get_tile(current_row, current_column)
+        return Move.Move(queen, destination_row, destination_column)
 
 
 
