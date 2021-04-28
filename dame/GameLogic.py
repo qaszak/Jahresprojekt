de4161868d2098345f496b267faf169038c48ec3
Lucas Vossen <lucas.vossen@hotmail.de>
2021-04-28 09:27:38 +0200

    change method visibility to public to enable calling by testcases.py

--- dame/GameLogic.py
+++ dame/GameLogic.py
@@ -1,17 +1,29 @@
+import DameBoard
+import Queen
+import Move
+
 class GameLogic:
     # ToDos:
     # - implement as a singleton
     # - implement methods
 
     __board = None
-    __queen = None
-    __x = -1
-    __y = -1
+    __move = None
 
     # public methods
-    def is_move_valid(self, board, queen, x, y):
-        self.__set_move(self, board, queen, x, y)
+    def set_move(self, board, move):
+        self.__board = board
+        self.__move = move
+
+    def is_queen_selected(self):
+        return not(self.__move.get_queen() is None)
+
+    def is_players_turn(self, player):
+        return self.__move.get_queen().get_player() == player
+
+    def is_move_valid(self):
         pass
+
 
     def is_hitting_move(self):
         pass
@@ -20,31 +32,40 @@ class GameLogic:
         pass
 
     # private methods
-    def __set_move(self, board, queen, x, y):
-        self.__board = board
-        self.__queen = queen
-        self.__x = x
-        self.__y = y
+    def is_direction_valid(self):
+        return self.is_row_direction_valid(self) and self.is_column_direction_valid(self)
 
-    def __is_direction_valid(self):
-        return self.__is_x_direction_valid(self) and self.__is_x_direction_valid(self)
+    def is_row_direction_valid(self):
+        player = self.__move.get_queen().get_player()
+        if player == 1:
+            return self.__move.get_row() - self.__move.get_queen().get_row() in [1, 2]
+        elif player == 2:
+            return self.__move.get_row() - self.__move.get_queen().get_row() in [-1, -2]
+        else:
+            return False
 
-    def __is_x_direction_valid(self):
+    def is_column_direction_valid(self):
         pass
 
-    def __is_y_direction_valid(self):
+    def is_move_blocked(self):
+        return self.is_move_blocked_by_own_queen(self) and \
+               self.is_move_blocked_by_opposing_queen(self) and \
+               self.is_move_blocked_by_border(self)
+
+    def is_move_blocked_by_own_queen(self):
         pass
 
-    def __is_move_blocked(self):
-        return self.__is_move_blocked_by_own_queen(self) and \
-               self.__is_move_blocked_by_opposing_queen(self) and \
-               self.__is_move_blocked_by_border(self)
-
-    def __is_move_blocked_by_own_queen(self):
+    def is_move_blocked_by_border(self):
         pass
 
-    def __is_move_blocked_by_border(self):
+    def is_move_blocked_by_opposing_queen(self):
         pass
 
-    def __is_move_blocked_by_opposing_queen(self):
-        pass
+
+    # temporary method for testing purposes
+    def print_logic_checks(self, player_turn):
+        is_queen_selected = self.is_queen_selected()
+        print("queen selected: " + str(is_queen_selected))
+        if is_queen_selected:
+            print("is players turn: " + str(self.is_players_turn(player_turn)))
+            print("row direction is valid: " + str(self.is_row_direction_valid()))
