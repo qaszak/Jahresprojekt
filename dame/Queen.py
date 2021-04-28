df2176140e858b692997efd384fc7ad882062053
Lucas Vossen <lucas.vossen@hotmail.de>
2021-04-28 04:29:02 +0200

    add function get_row and get_column

--- dame/Queen.py
+++ dame/Queen.py
@@ -1,18 +1,24 @@
 class Queen:
-    __x = -1
-    __y = -1
+    __row = -1
+    __column = -1
     __player = -1
 
-    def __init__(self, x, y, player):
-        self.set_position(x, y)
+    def __init__(self, row, column, player):
+        self.set_position(row, column)
         self.__player = player
 
-    def set_position(self, x, y):
-        self.__x = x
-        self.__y = y
+    def set_position(self, row, column):
+        self.__row = row
+        self.__column = column
 
-    def get_y_direction(self):
-        return -1 if self.__player == 1 else 1
+    def get_row(self):
+        return self.__row
+
+    def get_column(self):
+        return self.__column
 
     def get_player(self):
         return self.__player
+
+    def get_row_direction(self):
+        return -1 if self.__player == 1 else 1
