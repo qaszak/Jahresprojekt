aede21a7eab8da4a980fc5453a087c7da67149b8
Zakaria <zakaria.qasmi0@gmail.com>
2021-04-28 02:35:08 +0200

    Rand Problem gefixt ;)

--- board.py
+++ board.py
@@ -27,8 +27,9 @@ class Board:
         self.PAGE_HEIGHT = 500  #
         self.COLOR1 = 'grey'  #
         self.COLOR2 = 'white'  #
+        self.rand= 5
         ###################
-        self.x1, self.y1, self.x2, self.y2 = 0, 0, self.BOARD_WIDTH, self.BOARD_WIDTH
+        self.x1, self.y1, self.x2, self.y2 = self.rand, self.rand, self.BOARD_WIDTH+self.rand, self.BOARD_WIDTH+self.rand
         self.color = self.COLOR1
         self.can = Canvas(master, width=self.PAGE_WIDTH, heigh=self.PAGE_HEIGHT, bg='ivory')
         b1 = Button(master, text='Create Game', command=self.board)
@@ -38,8 +39,9 @@ class Board:
         if self.spiel == "dame":
             b3 = Button(master, text='start dame ', command=self.fill_board_queen)
             b3.pack(side=RIGHT, padx=3, pady=3)
+        b1.pack(side=RIGHT, padx=3, pady=3)
         self.can.pack(side=TOP, padx=5, pady=5)
-        b1.pack(side=LEFT, padx=3, pady=3)
+
         master.mainloop()
 
     # create an empty board
@@ -51,7 +53,7 @@ class Board:
             i, ite, self.x1, self.x2 = i + 1, ite + 1, self.x1 + self.BOARD_WIDTH, self.x2 + self.BOARD_WIDTH
             if ite == self.BOARD_SIZE:
                 self.y1, self.y2 = self.y1 + self.BOARD_WIDTH, self.y2 + self.BOARD_WIDTH
-                i, ite, self.x1, self.x2 = i + 1, 0, 0, self.BOARD_WIDTH
+                i, ite, self.x1, self.x2 = i + 1, 0, self.rand, self.BOARD_WIDTH + self.rand
             if i % 2 == 0:
                 self.color = self.COLOR2
             else:
@@ -61,13 +63,13 @@ class Board:
     def fill_board_KI_pawns(self):
         ite, self.x3, self.x4, self.y3, self.y4 = 0, self.BOARD_WIDTH / 5, self.BOARD_WIDTH / 5, self.BOARD_WIDTH - self.BOARD_WIDTH / 5, self.BOARD_WIDTH - self.BOARD_WIDTH / 5
         while ite < self.BOARD_SIZE:
-            self.can.create_rectangle(self.x3, self.x4, self.y3, self.y4, fill='yellow')
+            self.can.create_rectangle(self.x3+self.rand, self.x4+self.rand, self.y3+self.rand, self.y4+self.rand, fill='yellow')
             ite, self.x3, self.x4, self.y3, self.y4 = ite + 1, self.x3 + self.BOARD_WIDTH, self.x4, self.y3 + self.BOARD_WIDTH, self.y4
 
     # draw the pawns on the player side
     def fill_board_player_pawns(self):
-        ite, self.x3, self.x4, self.y3, self.y4 = 0, self.BOARD_WIDTH / 5, self.BOARD_WIDTH * (
-                self.BOARD_SIZE - 1) + self.BOARD_WIDTH / 5, self.BOARD_WIDTH - self.BOARD_WIDTH / 5, self.BOARD_WIDTH * self.BOARD_SIZE - self.BOARD_WIDTH / 5
+        ite, self.x3, self.x4, self.y3, self.y4 = 0, self.BOARD_WIDTH / 5+self.rand, self.BOARD_WIDTH * (
+                self.BOARD_SIZE - 1) + self.BOARD_WIDTH / 5 +self.rand, self.BOARD_WIDTH - self.BOARD_WIDTH / 5 +self.rand, self.BOARD_WIDTH * self.BOARD_SIZE - self.BOARD_WIDTH / 5 +self.rand
 
         while ite < self.BOARD_SIZE:
             obj1 = self.can.create_oval(self.x3, self.x4, self.y3, self.y4, fill='red')
@@ -85,7 +87,7 @@ class Board:
         self.fill_board_player_queen()
 
     def fill_board_KI_queen(self):
-        ite, self.x3, self.x4, self.y3, self.y4 = 0, self.BOARD_WIDTH / 5, self.BOARD_WIDTH / 5, self.BOARD_WIDTH - self.BOARD_WIDTH / 5, self.BOARD_WIDTH - self.BOARD_WIDTH / 5
+        ite, self.x3, self.x4, self.y3, self.y4 = 0, self.BOARD_WIDTH / 5+self.rand, self.BOARD_WIDTH / 5+self.rand, self.BOARD_WIDTH - self.BOARD_WIDTH / 5+self.rand, self.BOARD_WIDTH - self.BOARD_WIDTH / 5+self.rand
         i = 0
         while i < 6:
             self.can.create_rectangle(self.x3, self.x4, self.y3, self.y4, fill='yellow')
@@ -95,9 +97,9 @@ class Board:
                                                           self.BOARD_WIDTH * 2 - self.BOARD_WIDTH / 5
 
     def fill_board_player_queen(self):
-        ite, self.x3, self.x4, self.y3, self.y4 = 0, self.BOARD_WIDTH / 5, self.BOARD_WIDTH * (
-                self.BOARD_SIZE - 2) + self.BOARD_WIDTH / 5, self.BOARD_WIDTH - self.BOARD_WIDTH / 5, self.BOARD_WIDTH * (
-                                                          self.BOARD_SIZE - 1) - self.BOARD_WIDTH / 5
+        ite, self.x3, self.x4, self.y3, self.y4 = 0, self.BOARD_WIDTH / 5+self.rand, self.BOARD_WIDTH * (
+                self.BOARD_SIZE - 2) + self.BOARD_WIDTH / 5+self.rand, self.BOARD_WIDTH - self.BOARD_WIDTH / 5+self.rand, self.BOARD_WIDTH * (
+                                                          self.BOARD_SIZE - 1) - self.BOARD_WIDTH / 5+self.rand
         i = 0
         while i < self.BOARD_SIZE:
             self.can.create_oval(self.x3, self.x4, self.y3, self.y4, fill='red')
@@ -112,8 +114,8 @@ class Board:
         j = self.last_selected[1]
         if i == -1: return
         print(" i   j  ", i, j)
-        self.can.create_rectangle(i * self.BOARD_WIDTH, j * self.BOARD_WIDTH, (i + 1) * self.BOARD_WIDTH,
-                                  (j + 1) * self.BOARD_WIDTH, outline='black')
+        self.can.create_rectangle(i * self.BOARD_WIDTH+self.rand, j * self.BOARD_WIDTH+self.rand, (i + 1) * self.BOARD_WIDTH+self.rand,
+                                  (j + 1) * self.BOARD_WIDTH+self.rand, outline='black')
         col = self.last_possible_moves[0][0]
         line = self.last_possible_moves[0][1]
 
@@ -122,8 +124,8 @@ class Board:
         else:
             c = 'white'
         print("last possible moves col   line  ", col, line, c)
-        self.can.create_rectangle(col * self.BOARD_WIDTH, line * self.BOARD_WIDTH, (col + 1) * self.BOARD_WIDTH,
-                                  (line + 1) * self.BOARD_WIDTH, fill=c)
+        self.can.create_rectangle(col * self.BOARD_WIDTH+self.rand, line * self.BOARD_WIDTH+self.rand, (col + 1) * self.BOARD_WIDTH+self.rand,
+                                  (line + 1) * self.BOARD_WIDTH+self.rand, fill=c)
 
     def selected_square(self, x, y):
         col = int(x / self.BOARD_WIDTH)
@@ -131,8 +133,8 @@ class Board:
         self.removeSelectionFromOthers()
         self.last_selected = [col, line]
         print("selected square ", col, line)
-        self.can.create_rectangle(col * self.BOARD_WIDTH, line * self.BOARD_WIDTH, (col + 1) * self.BOARD_WIDTH,
-                                  (line + 1) * self.BOARD_WIDTH,
+        self.can.create_rectangle(col * self.BOARD_WIDTH+self.rand, line * self.BOARD_WIDTH+self.rand, (col + 1) * self.BOARD_WIDTH+self.rand,
+                                  (line + 1) * self.BOARD_WIDTH+self.rand,
                                   outline='red')
         self.possible_moves()
 
@@ -141,8 +143,8 @@ class Board:
         line = self.last_selected[1] - 1
         self.last_possible_moves = [[col, line]]
         print("last possible moves 1 col   line  ", col, line)
-        obj1 = self.can.create_rectangle(col * self.BOARD_WIDTH, line * self.BOARD_WIDTH, (col + 1) * self.BOARD_WIDTH,
-                                         (line + 1) * self.BOARD_WIDTH, fill='green')
+        obj1 = self.can.create_rectangle(col * self.BOARD_WIDTH+self.rand, line * self.BOARD_WIDTH+self.rand, (col + 1) * self.BOARD_WIDTH+self.rand,
+                                         (line + 1) * self.BOARD_WIDTH+self.rand, fill='green')
         self.can.tag_bind(obj1, "<Button-1>", self.play_move)
 
     def show_possible_moves(self, event):
@@ -169,18 +171,18 @@ class Board:
 
         print("the played move col   line  ", col, line)
         # remove the highlight from the possible square
-        self.can.create_rectangle(col * self.BOARD_WIDTH, line * self.BOARD_WIDTH, (col + 1) * self.BOARD_WIDTH,
-                                  (line + 1) * self.BOARD_WIDTH, fill=c_oval)
+        self.can.create_rectangle(col * self.BOARD_WIDTH+self.rand, line * self.BOARD_WIDTH+self.rand, (col + 1) * self.BOARD_WIDTH+self.rand,
+                                  (line + 1) * self.BOARD_WIDTH+self.rand, fill=c_oval)
 
         # draw a new oval in the played square
         t=self.BOARD_WIDTH/5
-        obj1 = self.can.create_oval(col * self.BOARD_WIDTH +t, line * self.BOARD_WIDTH+t, (col + 1) * self.BOARD_WIDTH-t,
-                                    (line + 1) * self.BOARD_WIDTH-t, fill='red')
+        obj1 = self.can.create_oval(col * self.BOARD_WIDTH +t+self.rand, line * self.BOARD_WIDTH+t+self.rand, (col + 1) * self.BOARD_WIDTH-t+self.rand,
+                                    (line + 1) * self.BOARD_WIDTH-t+self.rand, fill='red')
         self.can.tag_bind(obj1, "<Button-1>", self.show_possible_moves)
         # remove the old oval from the last position
         line = line + 1
-        self.can.create_rectangle(col * self.BOARD_WIDTH, line * self.BOARD_WIDTH, (col + 1) * self.BOARD_WIDTH,
-                                  (line + 1) * self.BOARD_WIDTH, fill=c_rectangle)
+        self.can.create_rectangle(col * self.BOARD_WIDTH+self.rand, line * self.BOARD_WIDTH+self.rand, (col + 1) * self.BOARD_WIDTH+self.rand,
+                                  (line + 1) * self.BOARD_WIDTH+self.rand, fill=c_rectangle)
         # initialise the last selected Square
         self.last_selected = [-1, -1]
 
