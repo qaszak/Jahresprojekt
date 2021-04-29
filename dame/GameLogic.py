96dbbae315d711d1db63165e8a22c4f612d1efce
Lucas Vossen <lucas.vossen@hotmail.de>
2021-04-29 05:13:54 +0200

    implement methods

--- dame/GameLogic.py
+++ dame/GameLogic.py
@@ -15,57 +15,122 @@ class GameLogic:
         self.__board = board
         self.__move = move
 
+
     def is_queen_selected(self):
         return not(self.__move.get_queen() is None)
+
 
     def is_players_turn(self, player):
         return self.__move.get_queen().get_player() == player
 
-    def is_move_valid(self):
-        pass
+
+    # new
+    def get_possible_moves_for(self, queen, board):
+        moves = []
+        for move in self.__get_potential_moves_for(queen):
+            self.set_move(board, move)
+            if self.__is_move_valid():
+                moves.append(move)
+        print(moves) # testing
+        return moves
+
+    # new
+    def __get_potential_moves_for(self, queen):
+        moves = []
+        prefixes = [-1, 1]
+        for prefix_1 in prefixes:
+            for prefix_2 in prefixes:
+                for step in [1, 2]:
+                    move = Move.Move(queen, queen.get_row() + prefix_1 * step, queen.get_column() + prefix_2 * step)
+                    moves.append(move)
+        print(moves) # testing
+        return moves
+
+    # new
+    def __is_move_valid(self):
+        return self.__is_move_in_board() and \
+               self.__is_direction_valid() and \
+               not self.__is_move_blocked_by_queen() and \
+               (self.__is_standard_move() or self.is_hitting_move())
 
 
+    def __is_move_in_board(self):
+        index_range = range(0, self.__board.get_size())
+        valid_row_destination = self.__move.get_row() in index_range
+        valid_column_destination = self.__move.get_column() in index_range
+        return valid_row_destination and valid_column_destination
+
+
+    def __is_direction_valid(self):
+        return self.__is_row_direction_valid(self) and self.__is_column_direction_valid(self)
+
+
+    def __is_row_direction_valid(self):
+        player = self.__move.get_queen().get_player()
+        distance_rows = self.__move.get_row() - self.__move.get_queen().get_row()
+        if player == 1:
+            return distance_rows in [1, 2]      # valid downward move?
+        elif player == 2:
+            return distance_rows in [-1, -2]    # valid upward move?
+
+
+    def __is_column_direction_valid(self):
+        distance_columns = self.__move.get_column() - self.__move.get_queen().get_column()
+        valid_left_move = distance_columns in [-1, -2]
+        valid_right_move = distance_columns in [1, 2]
+        return valid_left_move or valid_right_move
+
+
+    def __is_move_blocked_by_queen(self):
+        content_destination_tile = self.__board.get_tile(self.__move.get_row(), self.__move.get_column())
+        return content_destination_tile is not None
+
+    # new
+    def __is_standard_move(self):
+        distance_columns = self.__move.get_column() - self.__move.get_queen().get_column()
+        return distance_columns in [-1, 1]
+
+    # new
     def is_hitting_move(self):
-        pass
+        return self.get_hit_queen() is not None
 
-    def is_game_won(self):
-        pass
+    # new
+    def get_hit_queen(self):
+        hit_queen = None
+        distance_rows = self.__move.get_row() - self.__move.get_queen().get_row()
+        distance_columns = self.__move.get_column() - self.__move.get_queen().get_column()
+        if distance_rows in [-2, 2] and distance_columns in [-2, 2]:
+            hit_row = (1 if distance_rows == 2 else - 1) + self.__move.get_queen().get_row()
+            hit_column = (1 if distance_columns == 2 else -1) + self.__move.get_queen().get_column()
+            hit_queen = self.__board.get_tile(hit_row, hit_column)
+        return hit_queen
 
-    # private methods
+
+###################### FUNCTIONS FOR TESTCASES.PY (UNCOMMENT TO RUN TESTCASES) ####################################
+
+    """
+    def get_potential_moves_for(self, queen):
+        return self.__get_potential_moves_for(queen)
+
+    def is_move_valid(self):
+        return self.__is_move_valid()
+    
+    def is_move_in_board(self):
+        return self.__is_move_in_board()
+
     def is_direction_valid(self):
-        return self.is_row_direction_valid(self) and self.is_column_direction_valid(self)
+        return self.__is_direction_valid()
 
     def is_row_direction_valid(self):
-        player = self.__move.get_queen().get_player()
-        if player == 1:
-            return self.__move.get_row() - self.__move.get_queen().get_row() in [1, 2]
-        elif player == 2:
-            return self.__move.get_row() - self.__move.get_queen().get_row() in [-1, -2]
-        else:
-            return False
+        return self.__is_row_direction_valid()
 
     def is_column_direction_valid(self):
-        pass
+        return self.__is_column_direction_valid()
 
-    def is_move_blocked(self):
-        return self.is_move_blocked_by_own_queen(self) and \
-               self.is_move_blocked_by_opposing_queen(self) and \
-               self.is_move_blocked_by_border(self)
+    def is_move_blocked_by_queen(self):
+        return self.__is_move_blocked_by_queen()
 
-    def is_move_blocked_by_own_queen(self):
-        pass
+    def is_standard_move(self):
+        return self.__is_standard_move()
 
-    def is_move_blocked_by_border(self):
-        pass
-
-    def is_move_blocked_by_opposing_queen(self):
-        pass
-
-
-    # temporary method for testing purposes
-    def print_logic_checks(self, player_turn):
-        is_queen_selected = self.is_queen_selected()
-        print("queen selected: " + str(is_queen_selected))
-        if is_queen_selected:
-            print("is players turn: " + str(self.is_players_turn(player_turn)))
-            print("row direction is valid: " + str(self.is_row_direction_valid()))
+    """
