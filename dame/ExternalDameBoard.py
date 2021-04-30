28444023246c73a443f48e8a0b50b330d578f381
Lucas Vossen <lucas.vossen@hotmail.de>
2021-04-30 07:37:59 +0200

    add attributes name_player_turn and character_player_turn

--- dame/ExternalDameBoard.py
+++ dame/ExternalDameBoard.py
@@ -6,22 +6,22 @@ class ExternalDameBoard:
     __score = -1
     __name_ai = ""
     __name_human = ""
-    __player_turn = ""
+    __name_player_turn = ""
+    __character_player_turn = ""
     __number_of_queens_ai = -1
     __number_of_queens_human = -1
     __ai_queen_character = ""
     __human_queen_character = ""
     __empty_tile_character = ""
 
-
-
-    def __init__(self, board, score, name_ai, name_human, player_turn, number_of_queens_ai, number_of_queens_human,
+    def __init__(self, board, score, name_ai, name_human, name_player_turn, character_player_turn, number_of_queens_ai, number_of_queens_human,
                  ai_queen_character, human_queen_character, empty_tile_character):
         self.__board = board
         self.__score = score
         self.__name_ai = name_ai
         self.__name_human = name_human
-        self.__player_turn = player_turn
+        self.__name_player_turn = name_player_turn
+        self.__character_player_turn = character_player_turn
         self.__number_of_queens_ai = number_of_queens_ai
         self.__number_of_queens_human = number_of_queens_human
         self.__ai_queen_character = ai_queen_character
@@ -46,8 +46,12 @@ class ExternalDameBoard:
         return self.__name_human
 
 
-    def get_player_turn(self):
-        return self.__player_turn
+    def get_name_player_turn(self):
+        return self.__name_player_turn
+
+
+    def get_character_player_turn(self):
+        return self.__character_player_turn
 
 
     def get_number_of_queens_ai(self):
